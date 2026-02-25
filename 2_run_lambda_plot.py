3720258a25d2e896397d2bccb90a3314bb745667
Suhaas <suhaas@Suhaas>
2026-02-25 13:49:28 -0600

    added

---
+++ 2_run_lambda_plot.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt("lambda_results.txt", skiprows=1)
+
+moi = data[:,0]
+stealth = data[:,1]
+hijack = data[:,2]
+
+plt.figure(figsize=(8,5))
+plt.plot(moi, stealth, marker='o', label="Stealth (cI2 ≥ 145)")
+plt.plot(moi, hijack, marker='s', label="Hijack (Cro2 ≥ 55)")
+
+plt.xlabel("Multiplicity of Infection (MOI)")
+plt.ylabel("Probability (%)")
+plt.title("Lambda Phage Decision Probability vs MOI")
+plt.legend()
+plt.grid(True)
+
+plt.savefig("lambda_plot.png", dpi=300)
+plt.show()
+
